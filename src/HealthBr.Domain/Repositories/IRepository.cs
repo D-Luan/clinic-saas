4cea3633b477dd082f3fc75e66f6042be6400e96
D-Luan <78823769+D-Luan@users.noreply.github.com>
2026-09-13 13:01:31 -0300

    feat: add domain foundations (BaseEntity, enums, repository interface) (#2)
    * feat: add domain foundations (BaseEntity, enums, repository interface)
    
    Refs: task 1.1
    
    * docs: mark task 1.1 as done

---
+++ src/HealthBr.Domain/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+using HealthBr.Domain.Entities;
+
+namespace HealthBr.Domain.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<T>> ListAsync(CancellationToken cancellationToken = default);
+
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
+
+    void Update(T entity);
+
+    void Remove(T entity);
+}
